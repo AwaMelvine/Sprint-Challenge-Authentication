23447928cdddda7e4df18620c21c6f69a9933da9
Awa Melvine <melvineawa@gmail.com>
2019-07-26 11:12:26 +0100

    Implement front end register

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import Login from './components/Login';
 import Jokes from './components/Jokes';
-import Register from './components/Register';;
+import Register from './components/Register';
+
+const StyledNav = styled.div`
+  display: flex;
+  
+  a {
+    margin: 10px;
+    color: darkgreen;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &.active {
+      text-decoration: underline;
+    }
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <StyledNav className='nav'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/login'>Login</NavLink>
+        <NavLink to='/register'>Register</NavLink>
+      </StyledNav>
       <Route exact path='/' render={() => <Jokes />} />
       <Route path='/register' render={() => <Register />} />
       <Route path='/login' render={() => <Login />} />
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,10 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3300/api';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: 'Awa',
+            password: 'mel'
+        };
+    }
+
+    change = e => this.setState({ [e.target.name]: e.target.value })
+
+    submit = async e => {
+        e.preventDefault();
+        const { data } = await axios.post(`${apiUrl}/register`, this.state);
+        return <Redirect to='/' />;
+    }
+
     render() {
+        const { username, password } = this.state;
         return (
             <div>
                 <h2>Register</h2>
+                <form onSubmit={this.submit}>
+                    <div>
+                        <input name="username" value={username} onChange={this.change} />
+                    </div>
+                    <div>
+                        <input type="password" name="password" value={password} onChange={this.change} />
+                    </div>
+                    <div>
+                        <button type="submit">Register</button>
+                    </div>
+                </form>
             </div>
         )
     }
