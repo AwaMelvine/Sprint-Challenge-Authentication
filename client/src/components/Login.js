c6d8f2e5b078225d2a9af7415903721de5dc5331
Awa Melvine <melvineawa@gmail.com>
2019-07-26 12:00:21 +0100

    Style auth pages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,43 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
+import styled from 'styled-components';
+
 const apiUrl = 'http://localhost:3300/api';
+
+const StyledAuthDiv = styled.div`
+
+    width: 20%;
+    padding: 20px;
+
+    h2 { text-align: center; }
+
+    form {
+        width: 100%;
+
+        input {
+            width: 100%;
+            font-size: 1.2rem;
+            padding: 5px 10px;
+            outline: none;
+            margin-bottom: .5rem;
+            border-radius: 5px;
+            border: 1px solid #e0e0e0;
+        }
+        button {
+            width: 100%;
+            padding: 10px;
+            background: blueviolet;
+            border: 1px solid transparent;  
+            font-size: 1.1rem;
+            color: white;
+            border-radius: 5px; 
+        }
+
+    }
+
+`;
+
 
 class Login extends Component {
     constructor(props) {
@@ -30,7 +66,7 @@ class Login extends Component {
     render() {
         const { username, password } = this.state;
         return (
-            <div>
+            <StyledAuthDiv>
                 <h2>Login</h2>
                 <form onSubmit={this.submit}>
                     <div>
@@ -43,7 +79,7 @@ class Login extends Component {
                         <button type="submit">Login</button>
                     </div>
                 </form>
-            </div>
+            </StyledAuthDiv>
         )
     }
 }
