704c1e48296762ee15e8fbcbdb55b0eaddeffa9b
Awa Melvine <melvineawa@gmail.com>
2019-07-26 11:31:00 +0100

    Implement frontend jokes page

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3300/api';
 
 class Jokes extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            jokes: []
+        }
+    }
+    async componentDidMount() {
+        const token = await localStorage.getItem('token');
+        console.log(token);
+        const { data } = await axios.get(`${apiUrl}/jokes`, { headers: { authorization: token } });
+        console.log(data)
+        this.setState({ jokes: data });
+    }
     render() {
         return (
             <div>
                 <h2>Jokes</h2>
+                {this.state.jokes && this.state.jokes.map(joke => <p>{joke.joke}</p>)}
             </div>
         )
     }
